12419a872cecd1bcb34e566d573932bd90c4b7ef
adc001 <adc001@gmail.com>
2012-04-16 22:36:55 +0800

    newly created.

---
+++ SpeakingEnglish/src/info/liuqy/adc/speakingenglish/SpeakingEnglishActivity.java
@@ -0,0 +1,13 @@
+package info.liuqy.adc.speakingenglish;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpeakingEnglishActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
