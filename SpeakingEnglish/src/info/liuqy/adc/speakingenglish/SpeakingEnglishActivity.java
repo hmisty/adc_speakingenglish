00fb22b8ba88f24673ec09f91d9a36227d7e4748
adc001 <adc001@gmail.com>
2012-04-16 22:39:08 +0800

    use listview.

--- SpeakingEnglish/src/info/liuqy/adc/speakingenglish/SpeakingEnglishActivity.java
+++ SpeakingEnglish/src/info/liuqy/adc/speakingenglish/SpeakingEnglishActivity.java
@@ -1,9 +1,9 @@
 package info.liuqy.adc.speakingenglish;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class SpeakingEnglishActivity extends Activity {
+public class SpeakingEnglishActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
