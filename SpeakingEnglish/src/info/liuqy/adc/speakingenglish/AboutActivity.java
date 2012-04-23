c36cda405d075851c719268fc3014fb6e6e874bb
Evan Liu <qingyan123@gmail.com>
2012-04-23 23:00:27 +0800

    the about activity.

---
+++ SpeakingEnglish/src/info/liuqy/adc/speakingenglish/AboutActivity.java
@@ -0,0 +1,12 @@
+package info.liuqy.adc.speakingenglish;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
